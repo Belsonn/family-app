787f6ed8aa00a0f988ab98aec3b253197fcd07fb
Mateusz Belka <mateusz.belka14@gmail.com>
2021-01-24 04:14:04 +0100

    dodano select do widoku tasków

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,6 +1,8 @@
+import { FamilyService } from './../family.service';
 import { Task } from './../utils/tasks.models';
 import { TasksService } from './tasks.service';
 import { Component, OnInit } from '@angular/core';
+import { FamilyUser } from '../utils/family.models';
 
 @Component({
   selector: 'app-tasks',
@@ -11,13 +13,39 @@ export class TasksComponent implements OnInit {
   isLoading: boolean = false;
   tasks: Task[] = [];
 
-  constructor(private taskService: TasksService) {}
+  children: FamilyUser[] = [];
+
+  selectOptions = [$localize`Show all`];
+
+  constructor(
+    private taskService: TasksService,
+    private familyService: FamilyService
+  ) {}
 
   ngOnInit(): void {
     this.isLoading = true;
+    this.findChildren();
+    this.generateSelectOptions();
+    this.findAllTasks();
+
+  }
+
+  findAllTasks() {
     this.taskService.getTasks().subscribe((res) => {
       this.tasks = res.data.tasks;
       this.isLoading = false;
     });
+  }
+
+  findChildren() {
+    this.familyService.family.users.forEach((el) => {
+      el.role == 'child' ? this.children.push(el) : null;
+    });
+  }
+
+  generateSelectOptions() {
+    for (let i = 0; i < this.children.length; i++) {
+      this.selectOptions.push(this.children[i].name);
+    }
   }
 }
