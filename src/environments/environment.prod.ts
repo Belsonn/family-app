e4404cf247e7e8ccebe45531d0f34828cd73fe63
Mateusz Belka <mateusz.belka14@gmail.com>
2021-02-16 05:32:47 +0100

    dodano brakujący URL env

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiURL: 'https://family-app-backendd.herokuapp.com/api/v1/'
+  apiURL: 'https://family-app-backendd.herokuapp.com/api/v1/',
+  apiURLBasic: 'https://family-app-backendd.herokuapp.com/'
 };
